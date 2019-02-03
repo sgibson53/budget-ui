0d80c6194552318b82a51f379b5bfe3f4729cb04
Seth Gibson <wgibson53@gmail.com>
2019-02-03 12:13:24 -0600

    Core module import guard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header.component";
 import { HeaderNavComponent } from "./components/header/header-nav/header-nav.component";
 import { RouterModule } from "@angular/router";
+import { throwIfAlreadyLoaded } from "./module-import-guard";
 
 @NgModule({
   imports: [CommonModule, RouterModule],
   declarations: [HeaderComponent, HeaderNavComponent],
   exports: [HeaderComponent, HeaderNavComponent]
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, "CoreModule");
+  }
+}
